5da0804a8eb5747185253552a124aba3ddfc4f99
Andrea Nagy <andrea.nagy761201@gmail.com>
2024-05-06 20:36:59 +0200

    Add project files.

---
+++ BlazorApp_EF_CF/Data/MyDBContext.cs
@@ -0,0 +1,12 @@
+﻿using BlazorApp_EF_CF.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp_EF_CF.Data
+{
+    public class MyDBContext: DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options):base (options) { }
+
+        DbSet<User> Users { get; set; } 
+    }
+}
